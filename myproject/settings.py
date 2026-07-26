76ea3185787cda9839935cd6aef3d835e8ba6155
Vinit Jumani <jumanivinit90@gmail.com>
2026-07-26 18:38:29 +0530

    Set DEBUG to False for production

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "True") == "True"
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "vinit15.pythonanywhere.com",
